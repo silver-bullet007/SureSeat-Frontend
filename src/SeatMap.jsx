3c0fddb55be77d69ec1e027287c8ba640bb2d5f2
Lokesh <lokeshsundarav@gmail.com>
2026-09-17 16:34:52 +0530

    adding the seatMap component to display the available seats

---
+++ src/SeatMap.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getSeats, holdSeat } from './api';
+
+
+
+function SeatMap() {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+    const [seats, setSeats] = useState([]);
+    const [message, setMessage] = useState('');
+    const { id } = useParams();
+
+    useEffect(() => {
+        async function getAllSeats() {
+            try {
+                const data = await getSeats(id);
+                setSeats(data);
+            }
+            catch (err) {
+                setError(err.message);
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+        getAllSeats();
+    }, [id]);
+
+    async function handleClick(seatId) {
+        setMessage('');
+        setError('');
+        const token = localStorage.getItem(token);
+        try {
+            const result = await holdSeat(seatId, token);
+            setMessage(`Held Seat !! Expires at ${result.expiresAt}`);
+            const updated = await getSeats(id);
+            setSeats(updated);
+        }
+        catch (err) {
+            setError(err.messsage);
+        }
+
+    }
+
+    if (loading) return <p>Loading Seats...</p>
+
+    return (
+        <div>
+            <h2>Seats for Event {id}</h2>
+            {message && <p style={{ color: 'green' }}>{message}</p>}
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            <div>
+                {seats.map((seat) => (<button key={seat.id} disabled={seat.status !== 'AVAILABLE'} onClick={() => handleClick(seat.id)} >
+                    {seat.seatNumber} ({seat.status})
+                </button>))}
+            </div>
+        </div>
+    );
+}
+
+export default SeatMap;
